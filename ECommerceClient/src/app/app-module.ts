c610d6e86e235be3702c48705c9f51fc3b676407
kutayknt <130651773+kutayknt@users.noreply.github.com>
2026-04-16 20:40:57 +0300

    Custom Toastr Notification added.

--- ECommerceClient/src/app/app-module.ts
+++ ECommerceClient/src/app/app-module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { AdminModule } from './admin/admin-module';
 import { UiModule } from './ui/ui-module';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { UiModule } from './ui/ui-module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AdminModule, UiModule
+    AdminModule, UiModule,
+    ToastrModule.forRoot()
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
--- ECommerceClient/src/app/app.ts
+++ ECommerceClient/src/app/app.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, signal } from '@angular/core';
 import e from 'express';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrOptions, ToastrPosition } from './services/ui/custom-toastr.service';
+import { MessageType } from './services/admin/alertify.service';
 declare var $: any; //Buraya göz at $ is not defined hatası var.
 
 
@@ -10,7 +13,26 @@ declare var $: any; //Buraya göz at $ is not defined hatası var.
   styleUrl: './app.scss'
 })
 export class App{
+  constructor(private customToastr: CustomToastrService ) {
+    this.customToastr.message("Sipariş", "Sipariş", {
+      messageType: ToastrMessageType.Error,
+      position: ToastrPosition.BottomRight
+    })
+    this.customToastr.message("Sipariş", "Sipariş", {
+      messageType: ToastrMessageType.Info,
+      position: ToastrPosition.BottomLeft
+    })
+    this.customToastr.message("Sipariş", "Sipariş", {
+      messageType: ToastrMessageType.Success,
+      position: ToastrPosition.TopLeft
+    })
+    this.customToastr.message("Sipariş", "Sipariş", {
+      messageType: ToastrMessageType.Warning,
+      position: ToastrPosition.TopRight
+    })
+  }
   protected readonly title = signal('ECommerceClient');
+
 }
 // export class App implements OnInit{
   // ngOnInit(): void {
